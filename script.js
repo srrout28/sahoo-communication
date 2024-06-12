bfc86038428f9b03fb2a94b7c95eb393a7ccf560
Soumyaranjan2804 <soumyaranjanrout65@gmail.com>
2024-06-12 11:14:49 +0530

    ghoda pela

---
+++ script.js
@@ -0,0 +1,10 @@
+function showAlert() 
+{
+    alert("Call and Whatsapp- 9556757717");
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) 
+{
+    event.preventDefault();
+    alert('Thank you so much for your Message');
+});
